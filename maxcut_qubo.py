62132750f50a288aaac18ad10533b7e4e234e8ba
VridhiJ <vridhi@seas.upenn.edu>
2025-06-28 18:27:26 -0400

    now we move to converting graph to maxcut qubo

---
+++ maxcut_qubo.py
@@ -0,0 +1,12 @@
+import networkx as nx
+import matplotlib.pyplot as plt
+# -------------------------------
+# STEP 1: Build and Visualize Graph A (Complete Graph)
+# -------------------------------
+G = nx.complete_graph(4)
+pos = nx.spring_layout(G, seed=42)
+
+plt.figure(figsize=(4, 4))
+nx.draw(G, pos, with_labels=True, node_color='lightblue', node_size=600, edge_color='gray')
+plt.title("Graph A: Complete 4-Node")
+plt.show()
