0872f08798dc6529bf0b81e9cc50338ed4f8e371
VridhiJ <vridhi@seas.upenn.edu>
2025-06-28 18:24:39 -0400

    2 more graph types

--- qaoa_graphs.py
+++ qaoa_graphs.py
@@ -5,6 +5,14 @@ import matplotlib.pyplot as plt
 # Graph A: Complete graph with 4 nodes
 G_A = nx.complete_graph(4)
 
+# Graph B: Graph A minus one edge
+G_B = G_A.copy()
+G_B.remove_edge(0, 1)
+
+# Graph C: 5-node cycle with one added chord
+G_C = nx.cycle_graph(5)
+G_C.add_edge(0, 2)  # add a chord
+
 # Drawing function
 def draw_graph(G, title):
     pos = nx.spring_layout(G, seed=42)
@@ -15,3 +23,5 @@ def draw_graph(G, title):
 
 # Draw all graphs
 draw_graph(G_A, "Graph A: 4-node complete graph")    
+draw_graph(G_B, "Graph B:  graph missing edge 0–1")
+draw_graph(G_C, "Graph C: 5-node Circle + Chord")
